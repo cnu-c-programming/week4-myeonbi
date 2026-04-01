0aea88e6aff41d09f68c3bdf33d90c63ae8fdf1c
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-04-01 23:50:47 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -2,7 +2,7 @@
 
 int main() {
     int arr[] = {3, 7, 1, 9, 4, 6};
-    int n = sizeof(arr) / sizeod(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     for(int i=0; i<(n-1); i++) {
         void* addr_0 = &(arr[i]);
