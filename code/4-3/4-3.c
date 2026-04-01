6ffae25add9973e7a1f90a0268aa3116d814ce9a
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-04-01 23:54:53 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -9,7 +9,7 @@ int main() {
         void* addr_1 = &(arr[i+1]);
 
         printf("address of arr[%d]: %p\n", i, addr_0);
-        printf("address of arr[%d+1]: %p\n", i+1, addr_1);
+        printf("address of arr[%d+1]: %p\n", i, addr_1);
 
         printf("&(arr[%d]) - &(arr[%d+1]) = %d\n", i, i+1, (unsigned long long)addr_1 - (unsigned long long)addr_0);
         printf("\n");
